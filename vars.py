1c068b58d31c992b46188ccaa20e96fc625af6dc
fermani10 <31140793+fermani10@users.noreply.github.com>
2021-05-22 16:20:18 -0300

    Add files via upload

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ load_dotenv(find_dotenv())
 
 
 class var:
-    BOT_TOKEN = os.getenv("1819801273:AAE3gMgS8EG8kuT0ERL_IJDS3g4erRsM0is")  # from @botfather
-    API_ID = int(os.getenv("2327121"))  # from https://my.telegram.org/apps
-    API_HASH = os.getenv("e2c38ca4942c1b54735ce0c980841c5e")  # from https://my.telegram.org/apps
+    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
+    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
+    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
     START_MESSAGE = os.getenv("START_MESSAGE", None)  # Not Mandatory
